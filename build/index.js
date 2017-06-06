0444216c890e22ff21816f8a9ff027bb4c016fe3
Rodin <a@knoxlabs.com>
2017-06-06 16:31:56 +0000

    "update"

--- build/index.js
+++ build/index.js
@@ -12,21 +12,74 @@ System.register(['rodin/core', './src/Screen.js', './src/Socket.js'], function (
         }],
         execute: function () {
 
+            RODIN.start();
+
             var textureScene1 = RODIN.Loader.loadTexture('images/scene1.jpg');
             var textureScene2 = RODIN.Loader.loadTexture('images/scene2.jpg');
             var textureScene3 = RODIN.Loader.loadTexture('images/scene3.jpg');
 
+            const exp360 = new RODIN.Sculpt();
+            RODIN.Scene.add(exp360);
+
+            const sceneMain = new RODIN.Sphere(72, 36, 36, new THREE.MeshBasicMaterial({
+                map: textureScene1,
+                side: THREE.DoubleSide
+            }));
+
+            var hotspot1 = new RODIN.Plane(10, 5.555, new THREE.MeshBasicMaterial({
+                map: RODIN.Loader.loadTexture("images/hotspot.png"),
+                transparent: true
+            }));
+
+            var hotspot2 = new RODIN.Plane(10, 5.555, new THREE.MeshBasicMaterial({
+                map: RODIN.Loader.loadTexture("images/hotspot.png"),
+                transparent: true
+            }));
+
+            var hotspot3 = new RODIN.Plane(10, 5.555, new THREE.MeshBasicMaterial({
+                map: RODIN.Loader.loadTexture("images/hotspot.png"),
+                transparent: true
+            }));
+
+            var hotspot4 = new RODIN.Plane(10, 5.555, new THREE.MeshBasicMaterial({
+                map: RODIN.Loader.loadTexture("images/hotspot.png"),
+                transparent: true
+            }));
+
+            hotspot1.position.set(24, -10, 24);
+            hotspot1.rotation.x = -Math.PI / 2;
+            hotspot1.rotation.z = -Math.PI / 1.4;
+
+            hotspot2.position.set(-30, -10, -12);
+            hotspot2.rotation.x = -Math.PI / 2;
+            hotspot2.rotation.z = Math.PI / 2.3;
+
+            hotspot3.position.set(25, -10, 25);
+            hotspot3.rotation.x = -Math.PI / 2;
+            hotspot3.rotation.z = -Math.PI / 1.4;
+
+            hotspot4.position.set(-21, -10, -28);
+            hotspot4.rotation.x = -Math.PI / 2;
+            hotspot4.rotation.z = Math.PI / 0.5;
+
             function changeEnvPublic(texture, rot_angle) {
                 var textureSoc;
                 if (texture == 1) {
+                    exp360.remove(hotspot2);
+                    exp360.remove(hotspot3);
                     textureSoc = textureScene1;
+                    exp360.add(hotspot1);
                 } else if (texture == 2) {
                     exp360.remove(hotspot1);
+                    exp360.remove(hotspot4);
                     textureSoc = textureScene2;
                     exp360.add(hotspot2);
                     exp360.add(hotspot3);
                 } else if (texture == 3) {
+                    exp360.remove(hotspot2);
+                    exp360.remove(hotspot3);
                     textureSoc = textureScene3;
+                    exp360.add(hotspot4);
                 }
                 sceneMain._threeObject.material.map = textureSoc;
                 sceneMain.rotation.y = rot_angle;
@@ -40,53 +93,46 @@ System.register(['rodin/core', './src/Screen.js', './src/Socket.js'], function (
                 sceneMain.rotation.y = rot_angle;
                 var textureNum;
                 if (texture == textureScene1) {
+                    exp360.remove(hotspot2);
+                    exp360.remove(hotspot3);
                     textureNum = 1;
+                    exp360.add(hotspot1);
                 } else if (texture == textureScene2) {
+                    exp360.remove(hotspot1);
+                    exp360.remove(hotspot4);
                     textureNum = 2;
                     exp360.add(hotspot2);
                     exp360.add(hotspot3);
                 } else if (texture == textureScene3) {
+                    exp360.remove(hotspot2);
+                    exp360.remove(hotspot3);
                     textureNum = 3;
+                    exp360.add(hotspot4);
                 }
+
                 changeEnvSocket(textureNum, rot_angle);
             }
-
-            RODIN.start();
-
-            const exp360 = new RODIN.Sculpt();
-            RODIN.Scene.add(exp360);
-
-            var hotspot = new RODIN.Plane(10, 5.55, new THREE.MeshBasicMaterial({
-                map: RODIN.Loader.loadTexture("images/hotspot.png"),
-                transparent: true
-            }));
-
-            var hotspot1 = hotspot;
-            var hotspot2 = hotspot;
-            var hotspot3 = hotspot;
-            var hotspot4 = hotspot;
-
-            hotspot1.position.set(24, -10, 24);
-            hotspot1.rotation.x = -Math.PI / 2;
-            hotspot1.rotation.z = -Math.PI / 1.4;
-
-            hotspot2.position.set(24, -10, 24);
-            hotspot2.rotation.x = -Math.PI / 2;
-            hotspot2.rotation.z = -Math.PI / 1.4;
-
-            hotspot3.position.set(28, -10, 28);
-            hotspot3.rotation.x = -Math.PI / 2;
-            hotspot3.rotation.z = -Math.PI / 1.4;
-
-            const sceneMain = new RODIN.Sphere(72, 36, 36, new THREE.MeshBasicMaterial({
-                map: textureScene1
-            }));
 
             sceneMain.scale.set(-1, 1, 1);
             exp360.add(hotspot1);
 
             hotspot1.on(RODIN.CONST.GAMEPAD_BUTTON_DOWN, function (evt) {
-                exp360.remove(hotspot1);
+                //exp360.remove(hotspot1);
+                changeEnv(textureScene2, -Math.PI / 1.5);
+            });
+
+            hotspot2.on(RODIN.CONST.GAMEPAD_BUTTON_DOWN, function (evt) {
+                //exp360.remove(hotspot1);
+                changeEnv(textureScene1, 0);
+            });
+
+            hotspot3.on(RODIN.CONST.GAMEPAD_BUTTON_DOWN, function (evt) {
+                //exp360.remove(hotspot1);
+                changeEnv(textureScene3, -Math.PI / 1.5);
+            });
+
+            hotspot4.on(RODIN.CONST.GAMEPAD_BUTTON_DOWN, function (evt) {
+                //exp360.remove(hotspot1);
                 changeEnv(textureScene2, -Math.PI / 1.5);
             });
 
